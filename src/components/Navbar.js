b38dc10f5632a41340e86d29e6b222276b8906c5
Fariha-Rana <fariharana.eth@gmail.com>
2024-02-06 21:18:19 +0530

    added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 // Navbar.js
 
 import Link from "next/link";
+import Search from "./SearchBar";
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4 text-white">
       <ul className="flex justify-between">
         <li>Home / logo</li>
-        <li>Search</li>
+        <li><Search/></li>
         <div className="flex justify-end space-x-4">
         <li>Log in</li>
         {/* <li>My Playlist</li> */}
