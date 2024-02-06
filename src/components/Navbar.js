6861f792ed0e5cec42c9a6989721d582bb28ea66
Fariha-Rana <fariharana.eth@gmail.com>
2024-02-06 21:18:46 +0530

    added serachbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4 text-white">
       <ul className="flex justify-between">
-        <li>Home / logo</li>
+        <li>Home logo</li>
         <li><Search/></li>
         <div className="flex justify-end space-x-4">
         <li>Log in</li>
