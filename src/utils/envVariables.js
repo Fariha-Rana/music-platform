08471a943403bbbe61da4929885a54c50aceb126
Fariha-Rana <fariharana.eth@gmail.com>
2024-02-23 18:34:02 +0530

    updated some bugs

--- src/utils/envVariables.js
+++ src/utils/envVariables.js
@@ -1,11 +1,7 @@
 export const SPOTIFY_CLEINT_ID = process.env.SPOTIFY_CLEINT_ID;
 export const SPOTIFY_SECRET_TOKEN = process.env.SPOTIFY_SECRET_TOKEN;
-<<<<<<< HEAD
 // export const HOST_Name = "http://localhost:3000";  // in dev mode
 export const HOST_Name = "https://music-platform-six.vercel.app";  // in production mode
-=======
-export const HOST_Name = "https://music-platform-six.vercel.app";
->>>>>>> b9186de62b0491452ede9b2037b62f662260008a
 export const APPWRITE_PROJECT_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID;
 export const APPWRITE_DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
 export const APPWRITE_COLLECTION_ID = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID;
